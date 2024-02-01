aaf3870a773f1c9c4678e6d4061c77674a200a37
Jarlbatross <jaraane@gmail.com>
2024-02-01 10:44:47 +0100

    Structured views
    Reorganized the file/folder structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import JobsView from '../views/JobsView.vue'
 
 const routes = [
   {
@@ -12,7 +13,12 @@ const routes = [
     path: '/about',
     name: 'about',
     component: AboutView
-  }
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    component: JobsView
+  },
 ]
 
 const router = createRouter({
